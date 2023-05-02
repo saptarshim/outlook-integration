4fbf1619a7f988a0997343e57e2d3a668f38a23f
Saptarshi Mondal <saptarshi.mondal@gmail.com>
2023-05-01 22:57:03 -0700

    Initial commit with basi info changed from the sample plugin

--- main.ts
+++ main.ts
@@ -17,9 +17,9 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Outlook Integration', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('Get the Calnder from Outlook');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
